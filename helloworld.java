a01d8bc3a81fa3fde02b13c5bc8ffccd53c9b268
studyjava01 <117891925+studyjava01@users.noreply.github.com>
2022-11-10 14:32:44 +0800

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+/*
+	注释如果报错，在java 后面加上-encoding UTF-8
+*/
+
+public class main{
+	public static void main(String[] args){
+		System.out.println("hello world");
+	}
+}
---
+++ shujuleixing.java
@@ -0,0 +1,27 @@
+public class shujuleixing{
+	public static void main(String[] args){
+		//int
+		int i=1000;
+		//byte
+		byte b=1;
+		//short
+		short s=2;
+		//float
+		float f=3.14f;
+		//double
+		double d=3.1415926;
+		//char
+		char c='c';
+		//boolean
+		boolean bool=true;
+		
+		//print
+		System.out.println("int="+i+"\n");
+		System.out.println("byte="+b+"\n");
+		System.out.println("short="+s+"\n");
+		System.out.println("float="+f+"\n");
+		System.out.println("double="+d+"\n");
+		System.out.println("char="+c+"\n");
+		System.out.println("boolean="+bool+"\n");
+	}
+}
