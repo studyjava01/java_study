bab3ab8767dddd2badf84a403aea2431c78f6417
studyjava01 <117891925+studyjava01@users.noreply.github.com>
2022-11-10 14:56:22 +0800

    3.

---
+++ NameRule.java
@@ -0,0 +1,13 @@
+public class NameRule{
+	public static void main(String[] args){
+		/*
+			软性命名规范
+			类名：每个单词首字母大写（大驼峰）
+			方法名和变量名：首字母小写，后面每个单词首字母大写（小驼峰）
+			标识命名不能用中文形式
+		*/
+		int age=100;
+		int firstAge=100;
+		System.out.println(age+"\t"+firstAge);
+	}
+}
